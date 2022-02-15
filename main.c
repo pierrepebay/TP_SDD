132c7b89cf4a89e280272dd606969ab7d9a1855a
Taha Belkhiri <belkhiri.taha53@gmail.com>
2022-02-15 08:40:59 +0100

    fixed mem leak

--- main.c
+++ main.c
@@ -41,6 +41,7 @@ int main(int argc, char * argv[]){
           printAll(semaine_tete);
           saveListFile(semaine_tete, "test.txt");
           freeAll(semaine_tete);
+          fclose(file);
         }
     }
     else {
